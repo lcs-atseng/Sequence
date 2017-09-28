6d176a72d6a61baac29b0bb8833988a038e89df9
Alice Tseng <atseng@lcs-mba3235.local>
2017-09-28 15:28:49 -0400

    a panda without mouth

--- CanvasTestArea.playground/Contents.swift
+++ CanvasTestArea.playground/Contents.swift
@@ -22,11 +22,29 @@ import PlaygroundSupport
 
 // Create a new canvas
 let canvas = Canvas(width: 300, height: 500)
-// Draw a circle at the origin with radius of 50 pixels
-canvas.drawEllipse(centreX: 60, centreY: 350, width: 100, height: 100, borderWidth: 2)
+
+// ears and face
+canvas.drawEllipse(centreX: 55, centreY: 350, width: 100, height: 100, borderWidth: 2)
 canvas.drawEllipse(centreX: 245, centreY: 350, width: 100, height: 100, borderWidth: 2)
-// View the current state of the canvas
-canvas.drawEllipse(centreX: 150, centreY: 250, width: 200, height: 200)
+canvas.fillColor = Color.white
+canvas.drawEllipse(centreX: 150, centreY: 300, width: 230, height: 230)
+//eyes and nose
+canvas.fillColor = Color.black
+canvas.drawEllipse(centreX: 80, centreY: 300, width: 65, height: 50, borderWidth: 1)
+canvas.drawEllipse(centreX: 220, centreY: 300, width: 65, height: 50, borderWidth: 1)
+canvas.drawEllipse(centreX: 150, centreY: 260, width: 45, height: 30, borderWidth: 0)
+canvas.fillColor = Color.white
+canvas.drawEllipse(centreX: 90, centreY: 310, width: 20, height: 15, borderWidth: 0)
+canvas.drawEllipse(centreX: 210, centreY: 310, width: 20, height: 15, borderWidth: 0)
+canvas.fillColor = Color(hue: 20, saturation: 60, brightness: 90, alpha: 60)
+canvas.drawEllipse(centreX: 75, centreY: 260, width: 40, height: 25, borderWidth: 0)
+canvas.drawEllipse(centreX: 225, centreY: 260, width: 40, height: 25, borderWidth: 0)
+
+//mouth
+
+
+
+
 
 // This code is necessary to see the result in the Assistant Editor at right
 PlaygroundPage.current.liveView = canvas.imageView
