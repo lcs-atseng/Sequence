fe64c639ce81ad64a5c5a46a811f5bc94ed2da7b
Alice Tseng <atseng@lcs-mba3235.local>
2017-10-02 22:06:21 -0400

    finished panda

--- CanvasTestArea.playground/Contents.swift
+++ CanvasTestArea.playground/Contents.swift
@@ -24,26 +24,34 @@ import PlaygroundSupport
 let canvas = Canvas(width: 300, height: 500)
 
 // ears and face
-canvas.drawEllipse(centreX: 55, centreY: 350, width: 100, height: 100, borderWidth: 2)
-canvas.drawEllipse(centreX: 245, centreY: 350, width: 100, height: 100, borderWidth: 2)
+canvas.drawEllipse(centreX: 55, centreY: 350, width: 80, height: 80, borderWidth: 2)
+canvas.drawEllipse(centreX: 245, centreY: 350, width: 80, height: 80, borderWidth: 2)
+canvas.drawShapesWithBorders = false
 canvas.fillColor = Color.white
-canvas.drawEllipse(centreX: 150, centreY: 300, width: 230, height: 230)
-//eyes and nose
+canvas.drawEllipse(centreX: 150, centreY: 300, width: 220, height: 210)
+
+//eyes and red
+
 canvas.fillColor = Color.black
-canvas.drawEllipse(centreX: 80, centreY: 300, width: 65, height: 50, borderWidth: 1)
-canvas.drawEllipse(centreX: 220, centreY: 300, width: 65, height: 50, borderWidth: 1)
-canvas.drawEllipse(centreX: 150, centreY: 260, width: 45, height: 30, borderWidth: 0)
+canvas.drawEllipse(centreX: 90, centreY: 300, width: 45, height: 60, borderWidth: 2)
+canvas.drawEllipse(centreX: 205, centreY: 300, width: 45, height: 60, borderWidth: 2)
+canvas.fillColor = Color(hue: 25, saturation: 60, brightness: 90, alpha: 60)
+canvas.drawEllipse(centreX: 80, centreY: 255, width: 40, height: 20, borderWidth: 0)
+canvas.drawEllipse(centreX: 220, centreY: 255, width: 40, height: 20, borderWidth: 0)
 canvas.fillColor = Color.white
-canvas.drawEllipse(centreX: 90, centreY: 310, width: 20, height: 15, borderWidth: 0)
-canvas.drawEllipse(centreX: 210, centreY: 310, width: 20, height: 15, borderWidth: 0)
-canvas.fillColor = Color(hue: 20, saturation: 60, brightness: 90, alpha: 60)
-canvas.drawEllipse(centreX: 75, centreY: 260, width: 40, height: 25, borderWidth: 0)
-canvas.drawEllipse(centreX: 225, centreY: 260, width: 40, height: 25, borderWidth: 0)
+canvas.drawEllipse(centreX: 100, centreY: 310, width: 10, height: 10, borderWidth: 2)
+canvas.drawEllipse(centreX: 200, centreY: 310, width: 10, height: 10, borderWidth: 2)
+
+//nose
+canvas.fillColor = Color.black
+canvas.drawEllipse(centreX: 150, centreY: 270, width: 30, height: 20, borderWidth: 1)
 
 //mouth
-
-
-
+canvas.drawEllipse(centreX: 150, centreY: 250, width: 25, height: 25, borderWidth: 2)
+canvas.drawShapesWithBorders = false
+canvas.fillColor = Color.white
+canvas.drawEllipse(centreX: 142, centreY: 254, width: 13, height: 13, borderWidth: 2)
+canvas.drawEllipse(centreX: 158, centreY: 254, width: 13, height: 13, borderWidth: 2)
 
 
 // This code is necessary to see the result in the Assistant Editor at right
